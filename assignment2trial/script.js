05b03dba2d2fc4a4adadccf0de8cb515d98bc98b
anysas <105136670+anysas@users.noreply.github.com>
2025-04-17 11:06:29 +1000

    yuhyuhyuh

--- assignment2trial/script.js
+++ assignment2trial/script.js
@@ -1,21 +1,37 @@
-const video = document.querySelector("#custom-video-player");
+// Grabbing media player items
+const ambientWave = document.querySelector("#ambientwave");
 const playPauseBtn = document.querySelector("#play-pause-btn");
-const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
-function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+const playPauseBtnImg = document.querySelector("#play-pause-btn-img");
+const progressBarFill = document.querySelector("#progress-bar-fill");
+const progressBar = document.querySelector(".progressbar");
+const audioSpan = document.querySelector("#audio-time-span");
+
+// when play/pause button clicked, triggers playPause function
+playPauseBtn.addEventListener("click", playPause);
+
+// defining playPause function;
+function playPause() {
+  if (ambientWave.paused || ambientWave.ended) {
+    ambientWave.play();
+    playPauseBtnImg.src = "pause.png";
   } else {
-    video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    ambientWave.pause();
+    playPauseBtnImg.src = "play.png";
   }
 }
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
+
+// Event listener checking time of audio in order to update time bar according to audio position.
+ambientWave.addEventListener("timeupdate", updateProgressBar);
+
+function formatTime(seconds) {
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
 }
-// Add other functionalities here
+
+function updateProgressBar() {
+  const value = (ambientWave.currentTime / ambientWave.duration) * 100;
+  progressBarFill.style.width = value + "%";
+  audioSpan.textContent = formatTime(ambientWave.currentTime);
+  // audioSpan.textContent = ambientWave.currentTime.toFixed(2);
+}
