1ecb19cf1d93949b448eb9475e4b8222ff8fedde
anysas <105136670+anysas@users.noreply.github.com>
2025-04-22 11:00:03 +1000

    loop and volumebst

--- assignment2trial/script.js
+++ assignment2trial/script.js
@@ -5,6 +5,9 @@ const playPauseBtnImg = document.querySelector("#play-pause-btn-img");
 const progressBarFill = document.querySelector("#progress-bar-fill");
 const progressBar = document.querySelector(".progressbar");
 const audioSpan = document.querySelector("#audio-time-span");
+const volumeUp = document.querySelector("#vol-up");
+const volumeDown = document.querySelector("#vol-down");
+const loopBtn = document.querySelector("#loop-btn");
 
 // when play/pause button clicked, triggers playPause function
 playPauseBtn.addEventListener("click", playPause);
@@ -23,15 +26,53 @@ function playPause() {
 // Event listener checking time of audio in order to update time bar according to audio position.
 ambientWave.addEventListener("timeupdate", updateProgressBar);
 
-function formatTime(seconds) {
-  const mins = Math.floor(seconds / 60);
-  const secs = Math.floor(seconds % 60);
-  return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
-}
-
 function updateProgressBar() {
   const value = (ambientWave.currentTime / ambientWave.duration) * 100;
   progressBarFill.style.width = value + "%";
-  audioSpan.textContent = formatTime(ambientWave.currentTime);
-  // audioSpan.textContent = ambientWave.currentTime.toFixed(2);
+  audioSpan.textContent = ambientWave.currentTime.toFixed(2);
+}
+
+// volume buttons
+volumeUp.addEventListener("click", increaseVolume);
+
+function increaseVolume() {
+  if (ambientWave.volume < 0.9) {
+    ambientWave.volume += 0.1;
+  }
+}
+
+volumeDown.addEventListener("click", decreaseVolume);
+
+function decreaseVolume() {
+  if (ambientWave.volume > 0.11) {
+    ambientWave.volume -= 0.1;
+  }
+}
+
+// loop not initially on; define loop boolean
+let loop = false;
+
+// loop button clicked toggles boolean as on or off
+loopBtn.addEventListener("click", loopAudio);
+
+// when audio ended, replay triggered if loop is set to true
+ambientWave.addEventListener("ended", replay);
+
+// loop true or false toggle; scales down when loop is clicked to imitate button being pressed down on
+function loopAudio() {
+  if (loop) {
+    loop = false;
+    loopBtn.style.scale = "100%";
+  } else {
+    loop = true;
+    loopBtn.style.scale = "90%";
+  }
+}
+
+// replay function if loop is toggled
+function replay() {
+  if (loop) {
+    ambientWave.currentTime = 0;
+    ambientWave.play();
+  }
 }
