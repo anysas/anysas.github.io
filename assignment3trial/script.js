d28f96f3490d341efc6ccb5ddf06c0bd12a4ad2a
anysas <105136670+anysas@users.noreply.github.com>
2025-06-02 21:14:34 +1000

    notes

--- assignment3trial/script.js
+++ assignment3trial/script.js
@@ -74,33 +74,38 @@ const dropZone = document.querySelector("#dropzone");
 const bodiceBox = document.querySelector("#bodicebox");
 const menuHeading = document.querySelector("h2");
 
-// apply animation when ever
+// When finish button is clicked the website screen transforms into the end screen through items moving to the left side, having them disappear, and creating new text and button.
 finishBtn.addEventListener("click", function () {
+  // Both menus and dropzone moved to the side
   skirtMenu.classList.add("moveAside");
   bodiceMenu.classList.add("moveAside");
   dropZone.classList.add("moveAside");
+  // Removes finish button
   finishBtn.remove();
-
+  // Fades out the bodice menu as it moves to the side.
   bodiceBox.classList.add("clearOpacity");
-  finishBtn.classList.add("clearOpacity");
   menuHeading.classList.add("clearOpacity");
 
+  // creates a pink box to make it look like the bodice menu is being pulled over to the right. Creates background for the following text and button.
   const extendedMenu = document.createElement("div");
   extendedMenu.id = "extendedmenu";
   document.body.appendChild(extendedMenu);
   extendedMenu.style.left = "-700px";
   extendedMenu.classList.add("menuExtension");
 
+  // Create a button that takes users back to the starting screen.
   const makeAnother = document.createElement("button");
   makeAnother.id = "makeanother";
   document.body.appendChild(makeAnother);
   makeAnother.innerHTML = "Design another";
+  // Opacity starts at zero, and is applied showOpacity so it can fade in smoothly.
   makeAnother.classList.add("showOpacity");
   makeAnother.style.opacity = "0%";
+  // when the button is clicked it actually takes it back to the start.
   makeAnother.addEventListener("click", () => {
     window.location.href = "index.html";
   });
-
+  // Create text that signifies game ending; adds onto the unfinished/designer perspective as the clothes are in the making of a new Spring line.
   const comingSoon = document.createElement("h1");
   comingSoon.id = "comingsoon";
   document.body.appendChild(comingSoon);
@@ -109,6 +114,7 @@ finishBtn.addEventListener("click", function () {
   comingSoon.style.opacity = "0%";
 });
 
+// removes the bodice items completely once opacity to zero animation is finished so users don't accidentally change the bodice since it's only invisible and not completely gone.
 bodiceBox.addEventListener("animationend", () => {
   bodiceBox.remove();
 });
